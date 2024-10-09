42a3c344db51fc482870a5ca9f0d85cf6030cef6
Aryan Chugh <aryanchugh345@gmail.com>
2024-10-09 15:10:30 +0530

    fix(main.go): removed default subcommands

--- cmd/vc-analyze/main.go
+++ cmd/vc-analyze/main.go
@@ -12,6 +12,10 @@ var rootCmd = &cobra.Command{
     Use:     "vc-analyze",
     Version: "0.1.0",
     Short:   "A command-line tool for analyzing version control activity in Git repositories.",
+    
+    CompletionOptions: cobra.CompletionOptions{
+        DisableDefaultCmd: true,  // This line disables the 'completion' command
+    },
 }
 
 func init() {

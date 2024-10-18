6b477cd46a03a47c8a134d606fe3966552934ead
Nikhil Shrivastava <nikhil.shrivastava.8496@gmail.com>
2024-10-18 17:09:07 +0530

    Fix/7:Add CLI Help Documentation for all commands

--- cmd/vc-analyze/main.go
+++ cmd/vc-analyze/main.go
@@ -1,38 +1,37 @@
 package main
 
 import (
-    "os"
+	"os"
 
-    "github.com/spf13/cobra"
-    "github.com/adigulalkari/VC-Analyzer/cmd/subcommands"
-    "github.com/common-nighthawk/go-figure"
+	"github.com/adigulalkari/VC-Analyzer/cmd/subcommands"
+	"github.com/common-nighthawk/go-figure"
+	"github.com/spf13/cobra"
 )
 
 var rootCmd = &cobra.Command{
-    Use:     "vc-analyze",
-    Version: "0.1.0",
-    Short:   "A command-line tool for analyzing version control activity in Git repositories.",
-    
-    CompletionOptions: cobra.CompletionOptions{
-        DisableDefaultCmd: true,  // This line disables the 'completion' command
-    },
+	Use:     "vc-analyze",
+	Version: "0.1.0",
+	Short:   "A command-line tool for analyzing version control activity in Git repositories.",
+	CompletionOptions: cobra.CompletionOptions{
+		DisableDefaultCmd: true, // This line disables the 'completion' command
+	},
 }
 
 func init() {
-    subcommands.GetCmd.Flags().StringVarP(&subcommands.Repository, "repository", "r", "", "The GitHub repository in the format 'owner/repo'")
+	subcommands.GetCmd.Flags().StringVarP(&subcommands.Repository, "repository", "r", "", "The GitHub repository in the format 'owner/repo'")
+	rootCmd.AddCommand(subcommands.CmdCmd)
+	rootCmd.AddCommand(subcommands.GetCmd)
+	rootCmd.AddCommand(subcommands.CalcStatsCmd)
+	rootCmd.AddCommand(subcommands.AntiPatternsCmd)
+	rootCmd.AddCommand(subcommands.DetectBottlenecksCmd)
 
-    rootCmd.AddCommand(subcommands.GetCmd)
-    rootCmd.AddCommand(subcommands.CalcStatsCmd)
-    rootCmd.AddCommand(subcommands.AntiPatternsCmd)
-    rootCmd.AddCommand(subcommands.DetectBottlenecksCmd)
-    
 }
 func main() {
-    myFigure := figure.NewFigure("VC-Analyze", "", true)
-    myFigure.Print()
+	myFigure := figure.NewFigure("VC-Analyze", "", true)
+	myFigure.Print()
 
-    err := rootCmd.Execute()
-    if err != nil {
-        os.Exit(1)
-    }
+	err := rootCmd.Execute()
+	if err != nil {
+		os.Exit(1)
+	}
 }
--- pkg/analyzer/commit_history.go
+++ pkg/analyzer/commit_history.go
@@ -5,7 +5,6 @@ import (
 	"log"
 	"sort"
 
-	"strings"
 	"time"
 
 	"github.com/fatih/color"
